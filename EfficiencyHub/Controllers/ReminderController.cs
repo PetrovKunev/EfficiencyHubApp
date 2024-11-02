b6df037e3087dcb89cbebb43befcc88e839dddb2
Yavor Petrov <147103195+PetrovKunev@users.noreply.github.com>
2024-11-02 18:59:12 +0200

    Controllers

---
+++ EfficiencyHub/Controllers/ReminderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EfficiencyHub.Web.Controllers
+{
+    public class ReminderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
